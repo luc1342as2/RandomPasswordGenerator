87ae5fac1015756e07d130f2278842eca1805136
luc1342as2 <98180767+luc1342as2@users.noreply.github.com>
2024-05-13 18:46:45 -0400

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,29 @@
+
+const passwordBox = document.getElementById("password");
+const lenght = 12;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbol = "@#$%^&*()_+~|}{[]></-=";
+
+const allChars = upperCase + lowerCase + number + symbol;
+
+function createPassword(){
+   let password = "";
+   password += upperCase[Math.floor(Math.random() * upperCase.length)];
+   password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+   password += number[Math.floor(Math.random() * number.length)];
+   password += symbol[Math.floor(Math.random() * symbol.length)];
+
+   while(lenght > password.length){
+       password += allChars[Math.floor(Math.random() * allChars.length)];
+   }
+   passwordBox.value = password;
+}
+
+
+function copyPassword(){
+   passwordBox.select();
+   document.execCommand("copy");
+}
